85e78b26a82d0ddd10349333371367c678f6d681
elenahaejinkim <hj.elenakim@gmail.com>
2021-01-05 18:34:23 +0900

    과제 내용

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Router from "../Components/Router";
+
+function App(){
+    return(
+        <>
+            <Router/>
+        </>    
+    );
+}
+
+export default App;
